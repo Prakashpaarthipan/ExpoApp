18a6537ef16f6f975eb87a84714ef4c0ca42216d
prakashkovaidt <prakashkovaidt@gmail.com>
2020-11-11 10:47:23 +0530

    first commit

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,15 +1,27 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet,TouchableOpacity,TextInput } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
 export default function TabOneScreen() {
+	const [value, onChangeText] = React.useState('Useless Placeholder');
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>Welcome</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.js" />
+	  <TouchableOpacity
+        onPress={() => alert('Hello, world!')}
+        style={styles.button}>
+        <Text style={styles.buttonText}>Pick a photo</Text>
+      </TouchableOpacity>
+	  <TextInput
+      style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+      onChangeText={text => onChangeText(text)}
+      value={value}
+    />
     </View>
   );
 }
@@ -29,4 +41,13 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  button: {
+    backgroundColor: "blue",
+    padding: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
+  }
 });
